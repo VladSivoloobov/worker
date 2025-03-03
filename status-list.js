4cdccac706f0581a72c1870e603e127937dff1ab
Vladislav Sivolobov <sivolobovv3@gmail.com>
2025-03-04 01:36:33 +0300

    fixes

--- status-list.js
+++ status-list.js
@@ -1,11 +1,13 @@
 /**
  * @constant
  * @type {Array<string>}
+ * @description 410 urls list
  */
 export const goneList = [];
 
 /**
  * @constant
  * @type {Array<{from: string, to: string}>}
+ * @description 301 urls list
  */
 export const redirectList = [];
